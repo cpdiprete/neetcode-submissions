05f0fd1916e008e4ab18af161c07e65e2b9c7fad
Calvin DiPrete <156841549+cpdiprete@users.noreply.github.com>
2026-04-24 23:26:56 -0400

    Add: binary-tree-right-side-view - submission-4

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def __init__(self):
+        self.levelList = []
+    def levelOrder(self, root, level):
+        if root is None:
+            return [None]
+        if len(self.levelList) == level: # this is the first time this was seen
+            self.levelList.append(root.val)
+        right = self.levelOrder(root.right, level + 1)
+        left = self.levelOrder(root.left, level + 1)
+        return [None]
+
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        self.levelOrder(root, 0)
+        # print(self.levelList)
+        return self.levelList
+
+    
+        

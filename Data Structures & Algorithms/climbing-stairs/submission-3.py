a2af03adbe70f47cf849963279a83cb424f77ba9
Calvin DiPrete <156841549+cpdiprete@users.noreply.github.com>
2026-05-01 18:06:46 -0400

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n < 2:
+            return 1
+        dp = [None] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n + 1):
+            # 1) Jumping 2 spots from [i - 2], OR Jumping 1 spot twice from [i - 1]
+            # OR
+            # 2) Jumping 1 spot from [i - 1]
+            dp[i] = dp[i-2] + dp[i-1] 
+        print(dp)
+        return dp[n]
+        

0d43b55d54820ca30a2ddf7014126d4d97b56431
Calvin DiPrete <156841549+cpdiprete@users.noreply.github.com>
2026-05-11 14:50:00 -0400

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # initialize an empty set
+        # loop through the list, add to the set, or remove from the set if presetn
+        # return the element left in the set
+        dups = set()
+        for num in nums:
+            if num not in dups:
+                dups.add(num)
+            else:
+                dups.remove(num)
+        return list(dups)[0]
+
+        
